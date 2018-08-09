82fd1869e4e066363c89dac608fc19f5c5adebd8
gor <ruozhuo.chen@gmail.com>
2018-08-09 16:50:17 +1000

    Add files via upload

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,17 +1,34 @@
 wmname LG3D
-killall compton
+#terminusterminusterminusterminus killall compton
+killall syndaemon
+#killall xfce4-power-manager
 feh --bg-fill /home/rchen/wallpaper.jpg &
 pkill conky
-#killall systemGuard
-#killall checkEmail.php
+killall breedaniel 
+pkill -f checkEmail
+killall nm-applet
+killall -r slack 
 killall workrave
 killall background
-# killall -r slack
+killall pulseaudio
 
+# MatrixMonitors
+# BothMonitor
+if [ -f /home/rchen/.Xresources ]; then 
+    LaptopMonitor
+else
+    BothMonitor
+fi
+
+slack &
 workrave &
 background &
-# slack &
-# /home/rchen/bin/systemGuard &
-# /home/rchen/bin/checkEmail.php &
+/home/rchen/bin/breedaniel &
+/home/rchen/bin/checkEmail.sh &  
 /home/rchen/bin/startconky &
-compton &
+#xfce4-power-manager
+nm-applet & 
+# this is for spotify
+start-pulseaudio-x11 &
+syndaemon -i 1.0 -d -K 
+# compton &
